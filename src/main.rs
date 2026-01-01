888c9e05ca71f65459a57da9cce200fd0e3ae1dd
vladmeh <vladmeh@gmail.com>
2026-01-01 23:58:08 +0300

    Refactoring testing.

--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,6 @@ use email_newsletter::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    run()?.await
+    let listener = std::net::TcpListener::bind("127.0.0.1:8000")?;
+    run(listener)?.await
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,10 +1,12 @@
+use std::net::TcpListener;
+
 #[tokio::test]
 async fn health_check_work() {
-    spawn_app();
+    let address = spawn_app();
 
     let client = reqwest::Client::new();
     let response = client
-        .get("http://localhost:8000/health_check")
+        .get(format!("{}/health_check", &address))
         .send()
         .await
         .expect("Failed to execute request");
@@ -13,7 +15,11 @@ async fn health_check_work() {
     assert_eq!(Some(0), response.content_length());
 }
 
-fn spawn_app() {
-    let server = email_newsletter::run().expect("Failed to bind address");
+fn spawn_app() -> String {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    let port = listener.local_addr().unwrap().port();
+    let server = email_newsletter::run(listener).expect("Failed to bind address");
     tokio::spawn(server);
+
+    format!("http://127.0.0.1:{}", port)
 }
