fb1fc5f67b3a2e3181fdffd51f5c3bd8eea4f748
vladmeh <vladmeh@gmail.com>
2026-01-03 02:52:37 +0300

    Init db && create first migration

---
+++ migrations/20260102215133_create_subscriptions_table.sql
@@ -0,0 +1,7 @@
+-- Create Subscriptions Table
+CREATE TABLE subscriptions (
+    id uuid NOT NULL PRIMARY KEY,
+    email TEXT NOT NULL UNIQUE,
+    name TEXT NOT NULL,
+    subscribed_at timestamptz NOT NULL DEFAULT NOW()
+);
