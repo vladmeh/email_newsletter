ea86cc2157cdc685693542755e8feeb00bd92f7e
vladmeh <vladmeh@gmail.com>
2026-01-05 03:14:43 +0300

    Cleaning Up Initialisation.

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,12 @@
 use email_newsletter::config::get_configuration;
 use email_newsletter::startup::run;
+use email_newsletter::telemetry::{get_subscriber, init_subscriber};
 use sqlx::PgPool;
-
-use tracing::subscriber::set_global_default;
-use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
-use tracing_log::LogTracer;
-use tracing_subscriber::{EnvFilter, Registry, layer::SubscriberExt};
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    LogTracer::init().expect("Failed to set logger");
-
-    let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
-    let formatting_layer = BunyanFormattingLayer::new("newsletter".into(), std::io::stdout);
-    let subscriber = Registry::default()
-        .with(env_filter)
-        .with(JsonStorageLayer)
-        .with(formatting_layer);
-    set_global_default(subscriber).expect("Failed to set global default subscriber");
+    let subscriber = get_subscriber("email_newsletter".into(), "info".into());
+    init_subscriber(subscriber);
 
     let config = get_configuration().expect("Failed to read configuration");
     let connection_pool = PgPool::connect(&config.database.connection_string())
