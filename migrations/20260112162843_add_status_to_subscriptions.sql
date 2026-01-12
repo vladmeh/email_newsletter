36caf913927cdb323a4c0999dba2a994d89bb157
vladmeh <vladmeh@gmail.com>
2026-01-12 22:33:36 +0300

    Add column in subscription && new table for subscription tokens.

---
+++ migrations/20260112162843_add_status_to_subscriptions.sql
@@ -0,0 +1,2 @@
+-- Add status column to subscriptions table
+ALTER TABLE subscriptions ADD COLUMN status TEXT NULL;
---
+++ migrations/20260112171145_create_subscription_tokens_table.sql
@@ -0,0 +1,6 @@
+-- Create a new table for subscription tokens
+CREATE TABLE subscription_tokens (
+    subscription_token TEXT NOT NULL,
+    subscriber_id uuid NOT NULL REFERENCES subscriptions (id),
+    PRIMARY KEY (subscription_token)
+);
