847aac66a41b7f19546e7bb0afb0a5b98815b523
vladmeh <vladmeh@gmail.com>
2026-01-12 23:13:06 +0300

    Static Email.

--- tests/api/helpers.rs
+++ tests/api/helpers.rs
@@ -1,6 +1,7 @@
 use sqlx::{Connection, Executor, PgConnection, PgPool};
 use std::sync::LazyLock;
 use uuid::Uuid;
+use wiremock::MockServer;
 
 use email_newsletter::config::{DatabaseSettings, get_configuration};
 use email_newsletter::startup::{Application, get_connection_pool};
@@ -22,6 +23,7 @@ static TRACING: LazyLock<()> = LazyLock::new(|| {
 pub struct TestApp {
     pub address: String,
     pub db_pool: PgPool,
+    pub email_server: MockServer,
 }
 
 impl TestApp {
@@ -39,10 +41,13 @@ impl TestApp {
 pub async fn spawn_app() -> TestApp {
     LazyLock::force(&TRACING);
 
+    let email_server = MockServer::start().await;
+
     let config = {
         let mut conf = get_configuration().expect("Failed to read configuration");
         conf.database.database_name = Uuid::new_v4().to_string();
         conf.application.port = 0;
+        conf.email_client.base_url = email_server.uri();
         conf
     };
 
@@ -57,6 +62,7 @@ pub async fn spawn_app() -> TestApp {
     TestApp {
         address,
         db_pool: get_connection_pool(&config.database),
+        email_server,
     }
 }
 
--- tests/api/subscriptions.rs
+++ tests/api/subscriptions.rs
@@ -1,9 +1,18 @@
 use crate::helpers::spawn_app;
+use wiremock::matchers::{method, path};
+use wiremock::{Mock, ResponseTemplate};
 
 #[tokio::test]
 async fn subscribe_returns_a_200_for_valid_form_data() {
     let app = spawn_app().await;
     let body = "name=le%20guin&email=ursula_le_guin%40test.tt";
+
+    Mock::given(path("/email"))
+        .and(method("POST"))
+        .respond_with(ResponseTemplate::new(200))
+        .mount(&app.email_server)
+        .await;
+
     let response = app.post_subscription(body.into()).await;
 
     assert!(response.status().is_success());
@@ -57,4 +66,19 @@ async fn subscribe_returns_a_400_when_fields_are_present_but_invalid() {
             description
         )
     }
+}
+
+#[tokio::test]
+async fn subscribe_sends_a_confirmation_email_fo_valid_data() {
+    let app = spawn_app().await;
+    let body = "name=le%20guin&email=ursula_le_guin%40test.tt";
+
+    Mock::given(path("/email"))
+        .and(method("POST"))
+        .respond_with(ResponseTemplate::new(200))
+        .expect(1)
+        .mount(&app.email_server)
+        .await;
+
+    app.post_subscription(body.into()).await;
 }
