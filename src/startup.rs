b6b7a8437aa8962ee76f673e961821a86284c276
vladmeh <vladmeh@gmail.com>
2026-01-13 01:32:40 +0300

    Create routes subscriptions_confirm.

--- src/startup.rs
+++ src/startup.rs
@@ -7,7 +7,7 @@ use tracing_actix_web::TracingLogger;
 
 use crate::config::{DatabaseSettings, Settings};
 use crate::email_client::EmailClient;
-use crate::routes::{health_check, subscribe};
+use crate::routes::{confirm, health_check, subscribe};
 
 pub struct Application {
     port: u16,
@@ -67,6 +67,7 @@ pub fn run(
             .wrap(TracingLogger::default())
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
+            .route("/subscriptions/confirm", web::get().to(confirm))
             .app_data(db_pool.clone())
             .app_data(email_client.clone())
     })
