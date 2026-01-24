cf65aa0f559f8773a50f1823fdb3f8f27677ae3c
vladimir mikhaylov <vladmeh@gmail.com>
2026-01-25 02:34:53 +0300

    Update script init db && cargo fmt.

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -2,15 +2,10 @@
 set -x
 set -eo pipefail
 
-if ! [ -x "$(command -v psql)" ]; then
-  echo >&2 "Error: psql is not installed."
-  exit 1
-fi
-
 if ! [ -x "$(command -v sqlx)" ]; then
   echo >&2 "Error: sqlx is not installed."
   echo >&2 "Use:"
-  echo >&2 "    cargo install --version=0.8.6 sqlx-cli --no-default-features --features postgres"
+  echo >&2 "    cargo install --version='~0.8' sqlx-cli --no-default-features --features postgres"
   echo >&2 "to install it."
   exit 1
 fi
@@ -19,21 +14,35 @@ DB_USER=${POSTGRES_USER:=postgres}
 DB_PASSWORD="${POSTGRES_PASSWORD:=password}"
 DB_NAME="${POSTGRES_DB:=newsletter}"
 DB_PORT="${POSTGRES_PORT:=5432}"
+CONTAINER_NAME="newsletter_db"
 
 if [[ -z "${SKIP_DOCKER}" ]]
 then
-  docker run --name newsletter_db \
+  RUNNING_POSTGRES_CONTAINER=$(docker ps --filter 'name=postgres' --format '{{.ID}}')
+  if [[ -n $RUNNING_POSTGRES_CONTAINER ]]; then
+    echo >&2 "there is a postgres container already running, kill it with"
+    echo >&2 "     docker kill ${RUNNING_POSTGRES_CONTAINER}"
+    exit 1
+  fi
+  docker run \
+    --name "${CONTAINER_NAME}" \
     -e POSTGRES_USER="${DB_USER}" \
     -e POSTGRES_PASSWORD="${DB_PASSWORD}" \
     -e POSTGRES_DB="${DB_NAME}" \
+    --health-cmd="pg_isready -U ${DB_USER} || exit 1" \
+    --health-interval=1s \
+    --health-timeout=5s \
+    --health-retries=5 \
     -p "${DB_PORT}":5432 \
     -d postgres -N 1000
 fi
 
-export PGPASSWORD="${DB_PASSWORD}"
-until psql -h "localhost" -U "${DB_USER}" -p "${DB_PORT}" -d "postgres" -c '\q'; do
-  >&2 echo "Postgres is still unavailable - sleeping"
-  sleep 1
+until [ \
+    "$(docker inspect -f "{{.State.Health.Status}}" ${CONTAINER_NAME})" == \
+    "healthy" \
+  ]; do
+    >&2 echo "Postgres is still unavailable - sleeping"
+    sleep 1
 done
 
 >&2 echo "Postgres is up and running on port ${DB_PORT}!"
--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,13 +1,13 @@
 mod health_check;
 mod home;
+mod login;
 mod newsletter;
 mod subscriptions;
 mod subscriptions_confirm;
-mod login;
 
 pub use health_check::*;
 pub use home::*;
+pub use login::*;
 pub use newsletter::*;
 pub use subscriptions::*;
 pub use subscriptions_confirm::*;
-pub use login::*;
