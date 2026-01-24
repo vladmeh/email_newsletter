2ed96f9b8518b3a7b784882303729b5b9517e834
vladmeh <vladmeh@gmail.com>
2026-01-24 23:33:23 +0300

    Serving HTML Pages.

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,9 +1,11 @@
 mod health_check;
+mod home;
 mod newsletter;
 mod subscriptions;
 mod subscriptions_confirm;
 
 pub use health_check::*;
+pub use home::*;
 pub use newsletter::*;
 pub use subscriptions::*;
 pub use subscriptions_confirm::*;
