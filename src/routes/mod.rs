1f57a3aa4267fbaf6fb3441b0af28b029ee21e05
vladmeh <vladmeh@gmail.com>
2026-01-25 00:37:04 +0300

    Login form.

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -3,9 +3,11 @@ mod home;
 mod newsletter;
 mod subscriptions;
 mod subscriptions_confirm;
+mod login;
 
 pub use health_check::*;
 pub use home::*;
 pub use newsletter::*;
 pub use subscriptions::*;
 pub use subscriptions_confirm::*;
+pub use login::*;
--- src/startup.rs
+++ src/startup.rs
@@ -7,7 +7,7 @@ use tracing_actix_web::TracingLogger;
 
 use crate::config::{DatabaseSettings, Settings};
 use crate::email_client::EmailClient;
-use crate::routes::{confirm, health_check, home, publish_newsletter, subscribe};
+use crate::routes::{confirm, health_check, home, login_form, publish_newsletter, subscribe};
 
 pub struct Application {
     port: u16,
@@ -75,6 +75,7 @@ pub fn run(
         App::new()
             .wrap(TracingLogger::default())
             .route("/", web::get().to(home))
+            .route("/login", web::get().to(login_form))
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
             .route("/subscriptions/confirm", web::get().to(confirm))
