d9ece7eb011989608119795ab6faaf6baa694255
vladimir mikhaylov <vladmeh@gmail.com>
2026-01-25 03:29:35 +0300

    Login form. Post.

--- src/startup.rs
+++ src/startup.rs
@@ -7,7 +7,9 @@ use tracing_actix_web::TracingLogger;
 
 use crate::config::{DatabaseSettings, Settings};
 use crate::email_client::EmailClient;
-use crate::routes::{confirm, health_check, home, login_form, publish_newsletter, subscribe};
+use crate::routes::{
+    confirm, health_check, home, login, login_form, publish_newsletter, subscribe,
+};
 
 pub struct Application {
     port: u16,
@@ -76,6 +78,7 @@ pub fn run(
             .wrap(TracingLogger::default())
             .route("/", web::get().to(home))
             .route("/login", web::get().to(login_form))
+            .route("/login", web::post().to(login))
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
             .route("/subscriptions/confirm", web::get().to(confirm))
