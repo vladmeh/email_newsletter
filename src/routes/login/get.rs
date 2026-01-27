0358b13c969e044f0c3f775714e775facbda0c34
vladmeh <vladmeh@gmail.com>
2026-01-28 02:34:11 +0300

    Login Form. Delete cookies by reload page.

--- src/routes/login/get.rs
+++ src/routes/login/get.rs
@@ -1,3 +1,4 @@
+use actix_web::cookie::Cookie;
 use actix_web::http::header::ContentType;
 use actix_web::{HttpRequest, HttpResponse};
 
@@ -6,7 +7,7 @@ pub async fn login_form(request: HttpRequest) -> HttpResponse {
         None => "".into(),
         Some(cookie) => format!("<p><i>{}</i></p>", cookie.value()),
     };
-    HttpResponse::Ok()
+    let mut response = HttpResponse::Ok()
         .content_type(ContentType::html())
         .body(format!(
             r#"<!DOCTYPE html>
@@ -29,5 +30,9 @@ pub async fn login_form(request: HttpRequest) -> HttpResponse {
     </form>
 </body>
 </html>"#
-        ))
+        ));
+
+    response.add_removal_cookie(&Cookie::new("_flash", "")).unwrap();
+
+    response
 }
--- tests/api/login.rs
+++ tests/api/login.rs
@@ -17,4 +17,7 @@ async fn an_error_flash_message_is_set_on_failure() {
 
     let html_page = app.get_login_html().await;
     assert!(html_page.contains(r#"<p><i>Authentication failed</i></p>"#));
+
+    let html_page = app.get_login_html().await;
+    assert!(!html_page.contains(r#"<p><i>Authentication failed</i></p>"#));
 }
