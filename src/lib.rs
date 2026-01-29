2eda1bf339424487bc01b9b3e96d8063db85620e
vladmeh <vladmeh@gmail.com>
2026-01-30 01:41:01 +0300

    Session. Typed Session.

--- src/lib.rs
+++ src/lib.rs
@@ -3,5 +3,6 @@ pub mod config;
 pub mod domain;
 pub mod email_client;
 pub mod routes;
+pub mod session_state;
 pub mod startup;
 pub mod telemetry;
--- src/routes/login/post.rs
+++ src/routes/login/post.rs
@@ -1,6 +1,3 @@
-use crate::authentication::{AuthError, Credentials, validate_credentials};
-use crate::routes::error_chain_fmt;
-use actix_session::Session;
 use actix_web::error::InternalError;
 use actix_web::http::header::LOCATION;
 use actix_web::{HttpResponse, web};
@@ -8,6 +5,10 @@ use actix_web_flash_messages::FlashMessage;
 use secrecy::SecretString;
 use sqlx::PgPool;
 use std::fmt::Formatter;
+
+use crate::authentication::{AuthError, Credentials, validate_credentials};
+use crate::routes::error_chain_fmt;
+use crate::session_state::TypedSession;
 
 #[derive(serde::Deserialize)]
 pub struct FormData {
@@ -23,7 +24,7 @@ pub struct FormData {
 pub async fn login(
     form: web::Form<FormData>,
     pool: web::Data<PgPool>,
-    session: Session,
+    session: TypedSession,
 ) -> Result<HttpResponse, InternalError<LoginError>> {
     let credentials = Credentials {
         username: form.0.username,
@@ -36,7 +37,7 @@ pub async fn login(
             tracing::Span::current().record("user_id", tracing::field::display(&user_id));
             session.renew();
             session
-                .insert("user_id", user_id)
+                .insert_user_id(user_id)
                 .map_err(|e| login_redirect(LoginError::UnexpectedError(e.into())))?;
             Ok(HttpResponse::SeeOther()
                 .insert_header((LOCATION, "/admin/dashboard"))
