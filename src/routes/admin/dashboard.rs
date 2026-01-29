f0245fa1b5897a4b09d93552f993286cb8d887a2
vladmeh <vladmeh@gmail.com>
2026-01-30 02:02:20 +0300

    Session. Test admin dashboard.

--- src/routes/admin/dashboard.rs
+++ src/routes/admin/dashboard.rs
@@ -47,6 +47,6 @@ async fn get_username(user_id: Uuid, pool: &PgPool) -> Result<String, anyhow::Er
     let row = sqlx::query!(r#"SELECT username FROM users WHERE user_id = $1"#, user_id,)
         .fetch_one(pool)
         .await
-        .context("Failed to perfom a query to retrieve a username.")?;
+        .context("Failed to perform a query to retrieve a username.")?;
     Ok(row.username)
 }
