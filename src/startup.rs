f1e198ef5a7dfdc317cda551d293007518036688
vladmeh <vladmeh@gmail.com>
2026-01-30 00:47:37 +0300

    Session. Admin dashboard.

--- src/startup.rs
+++ src/startup.rs
@@ -14,7 +14,7 @@ use tracing_actix_web::TracingLogger;
 use crate::config::{DatabaseSettings, Settings};
 use crate::email_client::EmailClient;
 use crate::routes::{
-    confirm, health_check, home, login, login_form, publish_newsletter, subscribe,
+    admin_dashboard, confirm, health_check, home, login, login_form, publish_newsletter, subscribe,
 };
 
 pub struct Application {
@@ -107,6 +107,7 @@ pub async fn run(
             .route("/subscriptions", web::post().to(subscribe))
             .route("/subscriptions/confirm", web::get().to(confirm))
             .route("/newsletters", web::post().to(publish_newsletter))
+            .route("/admin/dashboard", web::get().to(admin_dashboard))
             .app_data(db_pool.clone())
             .app_data(email_client.clone())
             .app_data(base_url.clone())
