78c9364814652df6aca463bcdef6102bb8fdfee1
vladmeh <vladmeh@gmail.com>
2026-02-02 01:31:10 +0300

    Seed User. Log out.

--- src/routes/login/get.rs
+++ src/routes/login/get.rs
@@ -1,12 +1,12 @@
 use actix_web::HttpResponse;
 use actix_web::http::header::ContentType;
-use actix_web_flash_messages::{IncomingFlashMessages, Level};
+use actix_web_flash_messages::IncomingFlashMessages;
 use std::fmt::Write;
 
 pub async fn login_form(flash_messages: IncomingFlashMessages) -> HttpResponse {
-    let mut error_html = String::new();
-    for m in flash_messages.iter().filter(|m| m.level() == Level::Error) {
-        writeln!(error_html, "<p><i>{}</i></p>", m.content()).unwrap()
+    let mut msg_html = String::new();
+    for m in flash_messages.iter() {
+        writeln!(msg_html, "<p><i>{}</i></p>", m.content()).unwrap()
     }
 
     HttpResponse::Ok()
@@ -20,7 +20,7 @@ pub async fn login_form(flash_messages: IncomingFlashMessages) -> HttpResponse {
     <title>Login</title>
 </head>
 <body>
-    {error_html}
+    {msg_html}
     <form action="/login" method="post">
         <label>Username&nbsp;
             <input type="text" placeholder="Enter Username" name="username" autocomplete="username" required>
--- src/startup.rs
+++ src/startup.rs
@@ -14,8 +14,8 @@ use tracing_actix_web::TracingLogger;
 use crate::config::{DatabaseSettings, Settings};
 use crate::email_client::EmailClient;
 use crate::routes::{
-    admin_dashboard, change_password, change_password_form, confirm, health_check, home, login,
-    login_form, publish_newsletter, subscribe,
+    admin_dashboard, change_password, change_password_form, confirm, health_check, home, log_out,
+    login, login_form, publish_newsletter, subscribe,
 };
 
 pub struct Application {
@@ -111,6 +111,7 @@ pub async fn run(
             .route("/admin/dashboard", web::get().to(admin_dashboard))
             .route("/admin/password", web::get().to(change_password_form))
             .route("/admin/password", web::post().to(change_password))
+            .route("/admin/logout", web::post().to(log_out))
             .app_data(db_pool.clone())
             .app_data(email_client.clone())
             .app_data(base_url.clone())
