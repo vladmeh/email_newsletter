93a409ddd68a81f506a88320d6390213928d64f7
vladmeh <vladmeh@gmail.com>
2026-02-01 03:13:17 +0300

    Seed User. Confirm new password.

--- src/routes/admin/password/get.rs
+++ src/routes/admin/password/get.rs
@@ -2,12 +2,62 @@ use crate::session_state::TypedSession;
 use crate::utils::{e500, see_other};
 use actix_web::HttpResponse;
 use actix_web::http::header::ContentType;
+use actix_web_flash_messages::IncomingFlashMessages;
+use std::fmt::Write;
 
-pub async fn change_password_form(session: TypedSession) -> Result<HttpResponse, actix_web::Error> {
+pub async fn change_password_form(
+    session: TypedSession,
+    flash_messages: IncomingFlashMessages,
+) -> Result<HttpResponse, actix_web::Error> {
     if session.get_user_id().map_err(e500)?.is_none() {
         return Ok(see_other("/login"));
     }
+
+    let mut msg_html = String::new();
+    for m in flash_messages.iter() {
+        writeln!(msg_html, "<p><i>{}</i></p>", m.content()).unwrap();
+    }
+
     Ok(HttpResponse::Ok()
         .content_type(ContentType::html())
-        .body(include_str!("change_password_form.html")))
+        .body(format!(
+            r#"<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="content-type" content="text/html; charset=utf-8">
+    <title>Change Password</title>
+</head>
+<body>
+{msg_html}
+<form action="/admin/password" method="post">
+    <label>Current password
+        <input type="password"
+               name="current_password"
+               id="current_password"
+               placeholder="Enter current password"
+        >
+    </label>
+    <br>
+    <label>New password
+        <input type="password"
+               name="new_password"
+               id="new_password"
+               placeholder="Enter new password"
+        >
+    </label>
+    <br>
+    <label>Confirm new password
+        <input type="password"
+               name="confirm_new_password"
+               id="confirm_new_password"
+               placeholder="Confirm new password"
+        >
+    </label>
+    <br>
+    <button type="submit">Change password</button>
+</form>
+</body>
+</html>"#
+        )))
 }
--- src/routes/admin/password/post.rs
+++ src/routes/admin/password/post.rs
@@ -1,7 +1,8 @@
 use crate::session_state::TypedSession;
 use crate::utils::{e500, see_other};
 use actix_web::{HttpResponse, web};
-use secrecy::SecretString;
+use actix_web_flash_messages::FlashMessage;
+use secrecy::{ExposeSecret, SecretString};
 
 #[derive(serde::Deserialize)]
 pub struct FormData {
@@ -11,11 +12,18 @@ pub struct FormData {
 }
 
 pub async fn change_password(
-    _form: web::Form<FormData>,
+    form: web::Form<FormData>,
     session: TypedSession,
 ) -> Result<HttpResponse, actix_web::Error> {
     if session.get_user_id().map_err(e500)?.is_none() {
         return Ok(see_other("/login"));
+    }
+    if form.new_password.expose_secret() != form.confirm_new_password.expose_secret() {
+        FlashMessage::error(
+            "You entered two different new passwords - the field values must match.",
+        )
+        .send();
+        return Ok(see_other("/admin/password"));
     }
     todo!()
 }
