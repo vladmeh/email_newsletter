ffc9d9cb8a9ab3c737689ea51a575eceefde44af
vladmeh <vladmeh@gmail.com>
2026-02-05 23:29:20 +0300

    Idempotency. Retrieve Saved Responses.

--- src/routes/admin/newsletter/post.rs
+++ src/routes/admin/newsletter/post.rs
@@ -1,12 +1,13 @@
+use crate::authentication::UserId;
+use crate::domain::SubscriberEmail;
+use crate::email_client::EmailClient;
+use crate::idempotency::{IdempotencyKey, get_saved_response};
+use crate::utils::{e400, e500, see_other};
+use actix_web::web::ReqData;
 use actix_web::{HttpResponse, web};
 use actix_web_flash_messages::FlashMessage;
 use anyhow::Context;
 use sqlx::PgPool;
-
-use crate::domain::SubscriberEmail;
-use crate::email_client::EmailClient;
-use crate::idempotency::IdempotencyKey;
-use crate::utils::{e400, e500, see_other};
 
 #[derive(serde::Deserialize)]
 pub struct FormData {
@@ -16,19 +17,32 @@ pub struct FormData {
     idempotency_key: String,
 }
 
-#[tracing::instrument(name = "Publish a newsletter issue", skip(form, pool, email_client))]
+#[tracing::instrument(
+    name = "Publish a newsletter issue",
+    skip(form, pool, email_client, user_id)
+)]
 pub async fn publish_newsletter(
     form: web::Form<FormData>,
     pool: web::Data<PgPool>,
     email_client: web::Data<EmailClient>,
+    user_id: ReqData<UserId>,
 ) -> Result<HttpResponse, actix_web::Error> {
+    let user_id = user_id.into_inner();
     let FormData {
         title,
         text_content,
         html_content,
         idempotency_key,
     } = form.0;
-    let _idempotency_key: IdempotencyKey = idempotency_key.try_into().map_err(e400)?;
+
+    let idempotency_key: IdempotencyKey = idempotency_key.try_into().map_err(e400)?;
+    if let Some(saved_response) = get_saved_response(&pool, &idempotency_key, *user_id)
+        .await
+        .map_err(e500)?
+    {
+        return Ok(saved_response);
+    }
+
     let subscribers = get_confirmed_subscribers(&pool).await.map_err(e500)?;
     for subscriber in subscribers {
         match subscriber {
